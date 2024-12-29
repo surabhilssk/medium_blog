2d46c9a48bfb59f422f16f4d5424d193e87af623
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2024-12-29 17:18:11 +0530

    Implement custom hook

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { BACKEND_URL } from "../../config";
+
+export const useBlogs = () => {
+    const [loading, setLoading] = useState(true);
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+            .then(function(response){
+                setBlogs(response.data.blogs);
+                setLoading(false);
+            })
+    },[])
+
+    return {
+        loading,
+        blogs
+    }
+}
